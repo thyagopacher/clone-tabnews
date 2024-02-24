cd891aa5f619c2c1f6bdb8c7699660f0c40e1549
Thyago  Henrique Pacher <thyago.pacher@gmail.com>
2024-02-24 18:23:29 +0000

    Add endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,6 @@
 import "../../../../infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
   response.status(200).json({ chave: "123456789" });
 }
 
