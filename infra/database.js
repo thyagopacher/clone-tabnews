d3f212dae4bb7712e2e9bf3dff04134fc2a70603
Thyago  Henrique Pacher <thyago.pacher@gmail.com>
2024-03-22 11:03:24 +0000

    atualizar jtest para pegar .env

--- infra/database.js
+++ infra/database.js
@@ -32,5 +32,6 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA
     }
   }
-  return process.env.NODE_ENV === 'development' ? false : true
+  
+  return process.env.NODE_ENV === 'production' ? true : false;
 }
