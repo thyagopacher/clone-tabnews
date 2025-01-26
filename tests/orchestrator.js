5e51c6d92e7947dc719d012b2d4d3fd8c20d0ab2
thyagopacher <thyago.pacher@gmail.com>
2025-01-26 08:33:39 -0300

    up orchestrator

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,7 +5,8 @@ async function waitForAllServices() {
     
     async function waitForWebServer() {
         return retry(fetchStatusPage, {
-            retries: 100
+            retries: 100,
+            maxTimeout: 1000,
         });
 
         async function fetchStatusPage() {
