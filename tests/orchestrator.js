f516dbaa6e1fdcbbfe5a65c077030ef59375f660
thyagopacher <thyago.pacher@gmail.com>
2025-04-18 13:51:29 -0300

    fix

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,23 +1,23 @@
 import retry from "async-retry";
 
 async function waitForAllServices() {
-    await waitForWebServer();
-    
-    async function waitForWebServer() {
-        return retry(fetchStatusPage, {
-            retries: 100,
-            maxTimeout: 1000,
-        });
+  await waitForWebServer();
 
-        async function fetchStatusPage() {
-            const response = await fetch("http://localhost:3000/api/v1/status");
-            if (response.status !== 200) {
-                throw new Error("Status page not available");
-            }
-        }
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (response.status !== 200) {
+        throw new Error("Status page not available");
+      }
     }
+  }
 }
 
 export default {
-    waitForAllServices,
-}
+  waitForAllServices,
+};
